ab0f78ef0ba045d9a46760cc6483d0a1a8920b60
ccadmin <ccadmin@ccadmins-Mac-mini-2.local>
2017-08-28 17:30:35 +0900

    befor dinner

--- api/main.go
+++ api/main.go
@@ -29,6 +29,8 @@ type Routes []Route
 
 func main() {
 
+	log.Printf("hi")
+
 	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
@@ -55,10 +57,33 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Todo show:", todoId)
 }
 
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s\t%s\t%s\t%s",
+
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
+
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
