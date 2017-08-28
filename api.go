f6b8daf8d8ecfa62c9de8489621d4863b29970b2
ccadmin <ccadmin@ccadmins-Mac-mini-2.local>
2017-08-28 15:16:06 +0900

    adiing more routes

--- api.go
+++ api.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-    router := mux.NewRouter().StrictSlash(true)
-    // router.HandleFunc("/", Index)
-    router.HandleFunc("/hello", Hello)
-    // log.Fatal(http.ListenAndServe(":8080", router))
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/hello", Hello)
+	router.HandleFunc("/hello/{toshow}", ShowHello)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "first, %q", html.EscapeString(r.URL.Path))
-
+	fmt.Fprintf(w, "first, %q", html.EscapeString(r.URL.Path))
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello my api, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintln(w, "hello my api")
+}
 
+func ShowHello(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	toshow := vars["showhello"]
+	fmt.Fprintln(w, "ShowHello:", toshow)
 }
